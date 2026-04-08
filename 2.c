c2611934aea93280c2f8475c1e6918d821617c43
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-04-08 14:48:07 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,20 @@
+// WAP to check if a number is even or odd.
+  
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a Number=");
+    scanf("%d",&num);
+
+    if(num%2==0)
+    {
+        printf("number is Even \n");
+    }
+    else
+    {
+        printf("number is Odd \n");
+    }
+printf("Thank you");
+}
+
